f28d05a825b4c690a905194bef73e6707a418bbe
unknown <experte001@gmail.com>
2025-02-11 23:29:43 +0900

    UI enhancnment 🚀

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,25 @@
+function timeAgo(date) {
+  const now = new Date();
+  const past = new Date(date);
+  const seconds = Math.floor((now - past) / 1000);
+
+  const intervals = {
+    year: 31536000,
+    month: 2592000,
+    week: 604800,
+    day: 86400,
+    hour: 3600,
+    minut: 60,
+  };
+  for (const key in intervals) {
+    const interval = Math.floor(seconds / intervals[key]);
+    if (interval >= 1) {
+      return `${interval} ${key}${interval > 1 ? "s" : ""} ago`;
+    }
+  }
+  return "Just now";
+}
+
 // ADD NEW QUOTE UI MODAL OPEN CLOSE LOGIC
 const addQuoteContainer = document.querySelector(".add-quote-wrapper");
 const sidebarCreate = document.querySelectorAll("#create");
@@ -33,3 +55,11 @@ function handleOptionModal(quoteId) {
   const newUrl = `${window.location.pathname}?${params.toString()}`;
   history.pushState(null, "", newUrl);
 }
+
+//QUOTE CREATED AT DATE FORMATED
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("#quote-createdAt").forEach((element) => {
+    const timeString = element.getAttribute("createdAt");
+    element.textContent = timeAgo(timeString);
+  });
+});
